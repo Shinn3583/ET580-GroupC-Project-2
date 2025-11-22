30bef9881981698a5abf5c3bab15611dc90bac5c
G-Sencion45 <gabriel.sencion53@student.qcc.cuny.edu>
2025-11-22 14:40:55 +0000

    Commit 14 Gabriel Step 6 completed

--- Driver_Phase1.cpp
+++ Driver_Phase1.cpp
@@ -9,16 +9,16 @@ int main() {
 
     std::cout << "List initially empty? " << (L.empty() ? "yes" : "no") << "\n"; //Sikder
     std::cout << "Pushing front values 10, 20, 30...\n";                //Gabriel adding the push front and print 11/14/25
-    L.push_front(10);
-    L.push_front(20);
-    L.push_front(30);
+    //L.push_front(10);
+   // L.push_front(20);
+  //  L.push_front(30);
 
     std::cout << "List contents: ";
     L.print();
 
     std::cout << "Pushing back values 40, 50...\n";
-    L.push_back(40);
-    L.push_back(50);
+   // L.push_back(40);
+   // L.push_back(50);
 
     std::cout << "List contents: ";
     L.print();
@@ -26,18 +26,18 @@ int main() {
 // Sikder Phase 1 
      // Traverse via iterator
     std::cout << "Traverse with iterator: ";
-    for (Iterator it = L.begin(); it.hasNext(); it.next()) {
+   /* for (Iterator it = L.begin(); it.hasNext(); it.next()) {
         std::cout << it.getData() << ' ';
-    }
+    }*/
     std::cout << "\n";
 
     // Demonstrate find
-    Iterator pos = L.find(20);
+    /*Iterator pos = L.find(20);
     if (pos.hasNext()) {
         std::cout << "Found value: " << pos.getData() << "\n";
     } else {
         std::cout << "Value not found.\n";
-    }
+    }*/
 
     std::cout << "Phase I completed. \n";
     return 0;
